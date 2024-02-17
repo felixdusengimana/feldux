1b5fd6ac5f3daa97f63d1e2e8a7cac1ec3540bc3
Felix DUSENGIMANA <phelixdusengimana@gmail.com>
2024-02-17 22:19:39 +0200

    Update info.tsx

--- lib/info.tsx
+++ lib/info.tsx
@@ -6,8 +6,8 @@ export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey, I'm Felix. I'm the <b>Senior Software Developer at StylosConsults</b>{" "}
-      where my team helps companies build their apps.
+      Hey 👋, I'm Felix. I'm the <b>Software Developer</b>{" "}
+      You can reach me at <a href="mailto:info@feldux.com">info@feldux.com</a>
     </>
   );
 };
@@ -15,16 +15,7 @@ export const bio = () => {
   return (
     <>
       I am a software developer who is enthusiastic about creating solutions for
-      practical issues. My present employment is at{" "}
-      <a
-        href="https://www.stylosconsults.com"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-neutral-700 dark:text-neutral-200 hover:underline"
-      >
-        Stylosconsults
-      </a>
-      , where we assist organizations in constructing their applications.
+      practical issues.
     </>
   );
 };
