59ff81effc629e0c02c078a940c5a820e0dd1a37
felixdusengimana <phelixdusengimana@gmail.com>
2024-11-13 20:19:19 +0200

    new avatar

--- lib/info.tsx
+++ lib/info.tsx
@@ -1,13 +1,20 @@
 /* eslint-disable react/no-unescaped-entities */
-import me from "@/app/avatar.jpg";
+import me from "@/app/avatar.png";
 
 export const name = "Felix Dusengimana";
 export const avatar = me;
 export const about = () => {
   return (
     <>
-      Hey 👋, I'm Felix. I'm the <b>Software Developer</b><br/>
-      You can reach me at <a href="mailto:info@feldux.com" className="text-blue-400 underline hover:text-blue-800">info@feldux.com</a>
+      Hey 👋, I'm Felix. I'm the <b>Software Developer</b>
+      <br />
+      You can reach me at{" "}
+      <a
+        href="mailto:info@feldux.com"
+        className="text-blue-400 underline hover:text-blue-800"
+      >
+        info@feldux.com
+      </a>
     </>
   );
 };
